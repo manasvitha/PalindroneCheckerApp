603ed5c048448897f2e265d6fe46f66b2ee71483
manasvitha <manasvitha653@gmail.com>
2026-03-10 16:01:04 +0530

    uc9palindrome

--- PalinDromeApp/src/Palindrome.java
+++ PalinDromeApp/src/Palindrome.java
@@ -1,33 +1,26 @@
 public class Palindrome {
-import java.util.LinkedList;
 
 
         public static void main(String[] args) {
-            String input = "level";
+            String input = "madam";
 
-            LinkedList<Character> list = new LinkedList<>();
-
-            for (char c : input.toCharArray()) {
-                list.add(c);
-            }
-
-            boolean isPalindrome = true;
-
-            while (list.size() > 1) {
-                char fromFront = list.removeFirst();
-                char fromBack = list.removeLast();
-
-                if (fromFront != fromBack) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
+            boolean isPalindrome = check(input, 0, input.length() - 1);
 
             if (isPalindrome) {
                 System.out.println(input + " is a palindrome.");
             } else {
                 System.out.println(input + " is not a palindrome.");
             }
+        }
+
+        private static boolean check(String s, int start, int end) {
+            if (start >= end) {
+                return true;
+            }
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            return check(s, start + 1, end - 1);
         }
     }
 
